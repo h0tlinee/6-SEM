5da000421435ae378d408a764e6cf5ffff2109d3
h0tlinee <144935062+h0tlinee@users.noreply.github.com>
2024-02-10 16:44:25 +0700

    1 пункт 1 лабы

---
+++ Software development/lab1/lab1_sw_dvlp/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Converter;
+
+System.Console.WriteLine(Converter.Convert_10_p.Do(-186.54846, 16, 6));
