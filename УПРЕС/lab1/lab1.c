c4762aa5c82f8ec633562196cc73236ec7005490
Kirill Korablyov <pmi11.korablyov@gmail.com>
2024-02-26 18:11:26 +0700

    Файл на С, пока без сортировки по названию

--- УПРЕС/lab1/lab1.c
+++ УПРЕС/lab1/lab1.c
@@ -1,119 +1,76 @@
-#define _GNU_SOURCE
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #include <time.h>
-#include <errno.h>
+#include <linux/limits.h>
 
-int found = 0;                        // для отслеживания успеха, т.к. поиск рекурсивный и возвращать правильное значение тяжело
-int cmp(const void *a, const void *b) // функция сравнения для qsort, в алфавитном порядке
+void print_directory_contents(const char *path)
 {
-    return strcmp(((struct dirent *)a)->d_name, ((struct dirent *)b)->d_name);
-}
+    DIR *dir;
+    struct dirent *entry;
+    struct stat file_info;
 
-void listFilesRecursively(char *basePath, char *goal) // рекурсивный поиск, принимает каталог откуда поиск начинается и цель поиска
-{
-
-    char path[1000];              // буфер для того, чтобы формировать путь к каталогу
-    struct dirent *dp;            // структура для работы с каталогами
-    DIR *dir = opendir(basePath); // дескриптор каталога
-
-    // если каталог не получилось открыть, то возращаемся на уровень выше
-    if (!dir)
+    // Открытие каталога
+    dir = opendir(path);
+    if (dir == NULL)
     {
-        return;
+        perror("Ошибка открытия каталога");
+        exit(EXIT_FAILURE);
     }
 
-    while ((dp = readdir(dir)) != NULL) // пока каталог получилось успешно прочитать
+    // Печать имен каталогов
+    while ((entry = readdir(dir)) != NULL)
     {
-        if (strcmp(dp->d_name, goal) == 0) // если мы нашли заданный каталог
+        char file_path[PATH_MAX];
+        snprintf(file_path, sizeof(file_path), "%s/%s", path, entry->d_name);
+
+        if (stat(file_path, &file_info) == 0 && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, ".."))
         {
-            found = 1;               // цель найдена
-            struct dirent *buf[100]; // буфер для файлов, для посл. сортировки
-            struct stat st;          // структура для помещения информации stat
-            struct tm *timeinfo;     // структура для получения времени изменения файла
-            char buff[20];           // буфер для преобразования tm в строку и форматирования
-
-            strcat(basePath, "/");
-            strcat(basePath, dp->d_name); // прибавляем к пути найденный каталог
-            strcat(basePath, "/");
-
-            printf("%s  %s\n", "Заданный каталог:", dp->d_name);
-            printf("%s  %s\n\n", "Путь:", basePath); // вывод информации
-            dir = opendir(basePath);                 // открыли найденный каталог
-            printf("%s\n", "Файлы в каталоге:");
-            int i = 0;
-            while ((dp = readdir(dir)) != NULL)
+            if (S_ISDIR(file_info.st_mode))
             {
-                if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) // исключаем . и ..
-                {
-                    char a[100];
-                    strcpy(a, basePath);
-                    strcat(a, dp->d_name); // в этих 3 строках мы получаем полный путь, иначе, если файл не в одном каталоге, мы его не прочитаем
-                    if (stat(a, &st) == 0) // если удалось получить информацию о файле
-                    {
-                        if ((st.st_mode & S_IFDIR) == S_IFDIR) // смотрим, является ли файл каталогом по битовой маске
-                        {
-                            printf("%s\n", dp->d_name);
-                        }
-                        else
-                        {
-                            buf[i] = dp; // если не каталог, то заносим в буфер, для сортировки по имени
-                            i++;
-                        }
-                    }
-                    else
-                    {
-                        printf("%s\n", strerror(errno));
-                        printf("%s\n", "Не удалось получить информацию о файле! Возможно, необходимы права поиска во всех каталогах в полном пути к файлу");
-                    }
-                }
+                printf("%s (Каталог)\n", entry->d_name);
             }
-            qsort(buf, i, sizeof(struct dirent *), cmp); // сортировка
-
-            for (int j = 0; j < i; j++) // вывод информации о файлах
-            {
-                stat(buf[j]->d_name, &st);
-                timeinfo = localtime(&st.st_mtim);
-                strftime(buff, 20, "%b %d %H:%M", timeinfo);
-                printf("%s  %lu  %lu %s\n", (buf[j]->d_name), st.st_size, st.st_nlink, buff);
-                // printf("%s\n", buf[j]->d_name);
-            }
-            printf("\n");
-            closedir(dir);
-            return;
-        }
-
-        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
-        {
-            strcpy(path, basePath); // если не нашли каталог, то формируем новый путь и ищем дальше рекурсией
-            strcat(path, "/");
-            strcat(path, dp->d_name);
-            listFilesRecursively(path, goal);
         }
     }
-    closedir(dir);
-    return;
+
+    rewinddir(dir); // Сброс указателя чтения каталога
+
+    // Печать имен файлов
+    while ((entry = readdir(dir)) != NULL)
+    {
+        char file_path[PATH_MAX];
+        snprintf(file_path, sizeof(file_path), "%s/%s", path, entry->d_name);
+
+        if (stat(file_path, &file_info) == 0)
+        {
+            if (S_ISREG(file_info.st_mode))
+            {
+                time_t modified_time = file_info.st_mtime;
+                struct tm *modified_tm = localtime(&modified_time);
+                char modified_date[30];
+                strftime(modified_date, sizeof(modified_date), "%d.%m.%Y %H:%M:%S", modified_tm);
+
+                printf("%s (Файл, Длина: %ld, Дата изменения: %s, Ссылок: %lu)\n",
+                       entry->d_name, file_info.st_size, modified_date, file_info.st_nlink);
+            }
+        }
+    }
+
+    closedir(dir); // Закрытие каталога
 }
 
 int main(int argc, char *argv[])
 {
-
     if (argc != 2)
     {
-        printf("Программа принимает только 1 аргумент(искомый каталог)");
-        return 1;
+        printf("Использование: %s <каталог>\n", argv[0]);
+        return -1;
     }
-    char *goal = argv[1];
-    listFilesRecursively("/home", goal);
-    // printf("%d\n", found);
-    if (found != 1)
-    {
-        printf("%s\n", "Заданный каталог не был найден!");
-        return 1;
-    }
+
+    const char *directory = argv[1];
+    print_directory_contents(directory);
+
     return 0;
 }
