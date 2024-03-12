eaecfd65cdaacb866429acf16beb2e5de7554c39
Kirill Korablyov <pmi11.korablyov@gmail.com>
2024-03-13 03:36:23 +0700

    поправил тесты

--- УПРЕС/lab2/lab2_autotest.sh
+++ УПРЕС/lab2/lab2_autotest.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+#Стандартная работа программы
 echo "Запуск теста 1"
 ./lab2 4 9 > res1_exp
 if (diff res1_exp res1); then
@@ -6,6 +8,8 @@ if (diff res1_exp res1); then
 else
     echo "Тест 1: ошибка"
 fi
+
+#Ошибка ввода данных
 echo "Запуск теста 2"
 ./lab2 10 1 > res2_exp
 if (diff res2_exp res2); then
@@ -14,8 +18,9 @@ else
     echo "Тест 2: ошибка"
 fi
 
+#Неправильное использование программы
 echo "Запуск теста 3"
-./lab2 3 4 > res3_exp
+./lab2 > res3_exp
 if (diff res3_exp res3); then
     echo "Тест 3: успех"
 else
